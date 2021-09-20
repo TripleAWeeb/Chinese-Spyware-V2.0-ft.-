1b3f8209da541fdecf83d2887a656bcac8d12c36
TripleAWeeb <91071599+TripleAWeeb@users.noreply.github.com>
2021-09-20 17:49:19 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -57,47 +57,8 @@ module.exports = {
                 return message.channel.send(`:flag_cn: ***${song.title}$*** has been added to the propaganda list.`);
             }
         }
-        else if(cmd === 'p'){
-            if (!args.length) return message.channel.send('Air is not sufficent propaganda.');
-            let song = {};
-            if (ytdl.validateURL(args[0])) {
-                const song_info = await ytdl.getInfo(args[0]);
-                song = {title: song_info.videoDetails.title, url: song_info.videoDetails.video_url }
-
-            }else {
-                const video_finder = async (query) =>{
-                    const videoResult = await ytSearch(query);
-                    return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
-                }
-                const video = await video_finder(args.join(' '));
-                if (video){
-                    song = {title: video.title, url: video.url}
-                }else{
-                    message.channel.send('The result contains American propaganda and has been blocked by the supreme heavenly power of our chairman.');
-                }
-            }if (!server_queue){
-                const queue_constructor = {
-                    voice_channel: voice_channel,
-                    text_channel: message.channel,
-                    connection: null,
-                    songs: []
-                }
-                queue.set(message.guild.id, queue_constructor);
-                queue_constructor.songs.push(song);
-                try{
-                    const connection = await voice_channel.join();
-                    queue_constructor.connection = connection;
-                    video_player(message.guild, queue_constructor.songs[0]);
-                }catch (err) {
-                    queue.delete(message.guild.id);
-                    message.channel.send('MY BOT!!!! YOU KILLED MY BEAUTIFUL BOT!!!!!');
-                    throw err;
-                }
-            }else {
-                server_queue.songs.push(song);
-                return message.channel.send(`:flag_cn: ***${song.title}$*** has been added to the propaganda list.`);
-            }
-        }
+        
+        
         else if(cmd === 'skip') skip_song(message, server_queue);
         else if(cmd === 'stop') stop_song(message, server_queue);
         else if(cmd === 'fuckoff') stop_song(message, server_queue);
